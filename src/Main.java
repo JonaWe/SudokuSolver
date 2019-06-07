11da32aeaa47b53d22c091602ac1f7aced5e5115
jonaw <jonawessendorf@gmail.com>
2019-06-07 16:31:10 +0200

    initial

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import grid.Grid;
+
+/**
+ * created by Jona Wessendorf
+ * on 04.06.2019
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Grid g = new Grid();
+        g.createGrid();
+        g.printGrid();
+    }
+}
